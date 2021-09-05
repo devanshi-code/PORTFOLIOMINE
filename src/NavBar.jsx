a506c8f757fada28155335f0d17e8b7ccb15caa7
devanshi mishra <devu2732@gmail.com>
2021-09-05 23:52:33 +0530

    update

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-import  'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import { NavLink } from 'react-router-dom';
+// import  'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle';
 
-const NavBar=()=>{
+const NavBar = () => {
     return (
         <>
-        <div >
             <div >
                 <div >
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light navbar fixed-top scrolling-navbar Navbar_bgDark__3RVsz top-nav-collapse">
-                        <NavLink className="navbar-brand mx-auto" exact to="/" style={{padding:"10px",fontFamily:"'Berkshire Swash',cursive",fontSize:"1.8rem"}}>WELCOME</NavLink>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                    <div >
+                        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar fixed-top scrolling-navbar Navbar_bgDark__3RVsz top-nav-collapse">
+                            <NavLink className="navbar-brand mx-auto" exact to="/" style={{ padding: "10px", fontFamily: "'Berkshire Swash',cursive", fontSize: "1.8rem" }}>WELCOME</NavLink>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
 
-                        <div className="Collapse navbar-collapse " id="navbarSupportedContent">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <NavLink className="nav-link" exact activeClassName="menu_active" to="/">Home <span className="sr-only">(current)</span></NavLink>
-                                </li>
-                                <li className="nav-item ">
-                                    <NavLink className="nav-link" exact activeClassName="menu_active" to="/projects">Projects </NavLink>
-                                </li>
-                               
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" exact activeClassName="menu_active" to="/blogs">Blogs</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" exact activeClassName="menu_active" to="/experiences">Experiences</NavLink>
-                                </li>
-                            </ul>
-                        </div>
+                            <div className="Collapse navbar-collapse " id="navbarSupportedContent" style={{ marginLeft: "auto" }}>
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item active">
+                                        <NavLink className="nav-link" exact activeClassName="menu_active" to="/">Home <span className="sr-only">(current)</span></NavLink>
+                                    </li>
+                                    <li className="nav-item ">
+                                        <NavLink className="nav-link" exact activeClassName="menu_active" to="/projects">Projects </NavLink>
+                                    </li>
 
-                    </nav>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" exact activeClassName="menu_active" to="/blogs">Blogs</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" exact activeClassName="menu_active" to="/experiences">Experiences</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
